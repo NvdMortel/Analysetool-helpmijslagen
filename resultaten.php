604c3b808eddcf1009c130319084a0afff995370
Nico <nicovdmortel@hotmail.com>
2019-04-19 12:39:09 +0200

    Aanlevering werkbestand

---
+++ resultaten.php
@@ -0,0 +1,280 @@
+
+<?php 
+
+//json uit de database
+
+$results = '{"ec":{"blok1":{"oefenopgave1":{"punten":["2","2","1"],"log_start":"Thu Apr 11 2019 10:33:14 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 11 2019 10:33:42 GMT+0200 (Midden-Europese zomertijd)","punten_sessie2":["2","2","1"],"log_start_sessie2":"Thu Apr 11 2019 10:40:58 GMT+0200 (Midden-Europese zomertijd)","log_eind_sessie2":"Thu Apr 11 2019 10:41:06 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["1","1","2","2","2"],"log_start":"Thu Apr 11 2019 10:58:04 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 11 2019 11:00:34 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["1","0","0","1"],"log_start":"Thu Apr 11 2019 11:13:03 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 11 2019 11:15:34 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave4":{"punten":["3","0","2","0"],"log_start":"Thu Apr 11 2019 11:33:35 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 11 2019 11:34:54 GMT+0200 (Midden-Europese zomertijd)"}},"blok2":{"oefenopgave1":{"punten":["1"],"log_start":"Tue Apr 16 2019 11:19:31 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 11:20:31 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["2","0"],"log_start":"Tue Apr 16 2019 11:25:50 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 11:27:24 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["1","1"],"log_start":"Tue Apr 16 2019 11:32:22 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 11:33:43 GMT+0200 (Midden-Europese zomertijd)"}},"blok3":{"oefenopgave1":{"punten":["2"],"log_start":"Tue Apr 16 2019 11:52:00 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 11:52:15 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["1","2"],"log_start":"Tue Apr 16 2019 11:59:19 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 12:00:11 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["1"],"log_start":"Tue Apr 16 2019 12:08:06 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 12:08:43 GMT+0200 (Midden-Europese zomertijd)"}},"blok4":{"oefenopgave1":{"punten":["2","1"],"log_start":"Thu Apr 18 2019 15:23:17 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 15:23:59 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["2","0","1"],"log_start":"Thu Apr 18 2019 15:31:46 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 15:33:27 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["2","0"],"log_start":"Thu Apr 18 2019 15:40:24 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 15:41:09 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave4":{"punten":["2","1","2","1"],"log_start":"Thu Apr 18 2019 15:58:09 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 15:58:32 GMT+0200 (Midden-Europese zomertijd)"}}},"gs":{"blok1":{"oefenopgave1":{"punten":["1","0","4","0"],"log_start":"Sat Apr 13 2019 11:12:29 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:12:40 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["3","2"],"log_start":"Sat Apr 13 2019 11:15:47 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:16:58 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["1","2","2"],"log_start":"Sat Apr 13 2019 11:30:52 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:33:06 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave4":{"punten":["2"],"log_start":"Sat Apr 13 2019 11:35:47 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:36:17 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave5":{"punten":["2"],"log_start":"Sat Apr 13 2019 11:39:11 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:39:33 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave6":{"punten":["1","0"],"log_start":"Sat Apr 13 2019 11:44:13 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:45:11 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave7":{"punten":["0","3","1"],"log_start":"Sat Apr 13 2019 11:56:38 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 11:59:22 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave8":{"punten":["4","4","2","2"],"log_start":"Sat Apr 13 2019 12:18:41 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Sat Apr 13 2019 12:18:53 GMT+0200 (Midden-Europese zomertijd)"}},"blok2":{"oefenopgave1":{"punten":["2","1"],"log_start":"Tue Apr 16 2019 13:05:13 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:07:03 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["2"],"log_start":"Tue Apr 16 2019 13:11:46 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:13:32 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["3"],"log_start":"Tue Apr 16 2019 13:20:45 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:21:40 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave4":{"punten":["1","1"],"log_start":"Tue Apr 16 2019 13:24:34 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:25:44 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave5":{"punten":["0","0"],"log_start":"Tue Apr 16 2019 13:32:45 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:32:50 GMT+0200 (Midden-Europese zomertijd)","punten_sessie2":["2","1"],"log_start_sessie2":"Thu Apr 18 2019 13:12:38 GMT+0200 (Midden-Europese zomertijd)","log_eind_sessie2":"Thu Apr 18 2019 13:13:40 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave6":{"punten":["2","0"],"log_start":"Tue Apr 16 2019 13:38:27 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Tue Apr 16 2019 13:40:10 GMT+0200 (Midden-Europese zomertijd)"}},"blok3":{"oefenopgave1":{"punten":["2","3"],"log_start":"Thu Apr 18 2019 13:23:17 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 13:24:50 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave2":{"punten":["2","2","2"],"log_start":"Thu Apr 18 2019 13:33:39 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 13:34:36 GMT+0200 (Midden-Europese zomertijd)","punten_sessie2":["2","2","2"],"log_start_sessie2":"Thu Apr 18 2019 13:35:46 GMT+0200 (Midden-Europese zomertijd)","log_eind_sessie2":"Thu Apr 18 2019 13:35:58 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave3":{"punten":["2","1"],"log_start":"Thu Apr 18 2019 13:45:12 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 13:56:58 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave4":{"punten":["3","1"],"log_start":"Thu Apr 18 2019 14:05:00 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 14:06:27 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave5":{"punten":["2","3"],"log_start":"Thu Apr 18 2019 14:44:18 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 14:45:48 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave6":{"punten":["2","1"],"log_start":"Thu Apr 18 2019 14:49:36 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 14:50:20 GMT+0200 (Midden-Europese zomertijd)"},"oefenopgave7":{"punten":["2","1","0"],"log_start":"Thu Apr 18 2019 15:01:52 GMT+0200 (Midden-Europese zomertijd)","log_eind":"Thu Apr 18 2019 15:03:32 GMT+0200 (Midden-Europese zomertijd)"}}}}';
+
+	$results = json_decode($results, true); //json to php
+
+	if (isset($results['ec'])) {
+		$vak = "economie";
+		$symbol = 'ec';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'vwo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'havo';
+		}
+	}
+	else if (isset($results['na'])) {
+		$vak = "natuurkunde";
+		$symbol = 'na';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	} 
+	else if (isset($results['sk'])) {
+		$vak = "scheikunde";
+		$symbol = 'sk';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['bi'])) {
+		$vak = "biologie";
+		$symbol = 'bi';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['wia'])) {
+		$vak = "wiskunde-a";
+		$symbol = 'wia';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['wib'])) {
+		$vak = "wiskunde-b";
+		$symbol = 'wib';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['mo'])) {
+		$vak = "management-en-organisatie";
+		$symbol = 'mo';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['gs'])) {
+		$vak = "geschiedenis";
+		$symbol = 'gs';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['ak'])) {
+		$vak = "aardrijkskunde";
+		$symbol = 'ak';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['ne'])) {
+		$vak = "nederlands";
+		$symbol = 'ne';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+	else if (isset($results['engels'])) {
+		$vak = "engels";
+		$symbol = 'en';
+		if (array_count_values(array_column($results, 'opgavenaam')) == 28) { //aanpassen naar juist aantal
+			$schooltype = 'havo';
+			$schooltype = array_count_values(array_column($results, 'opgavenaam'));
+		}
+		else {
+			$schooltype = 'vwo';
+		}
+	}
+
+	//inladen gegevens
+	require_once 'examentrainingfiles/'.$vak.'-'.$schooltype.'.php'; 
+
+?>
+
+	<table id="myTable" class="table table-bordered">
+		<thead>
+			<tr>
+				<th>Blok</th>
+				<th>Domein</th>
+				<th>Opgave</th>
+				<th>Vraag</th>
+				<th>Max punten</th>
+				<th>Behaald</th>
+				<th>Cesuurscore</th>
+				<th>Cijfer</th>
+			</tr>
+		</thead>
+		<tfoot>
+
+		</tfoot>
+		<tbody>
+
+			<?php 
+			$opgavenaam = array();
+			$cesuurscores = array();
+			$aantalpunten = array();
+			$opgavecijfer = array();
+			$blokcijfer = array();
+
+			$blok = 1;
+			$opgaves = 1;
+
+
+			foreach ($oefenen as $oefenopgave) {
+
+				if ($oefenopgave['opgavenr'] != $opgaves) {
+					echo '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><i>N-term:</i>';
+					echo round(5.45-(9*array_sum($cesuurscores)/array_sum($aantalpunten)),1);
+					echo '</td><td><i>Cijfer:</i></td><td>';
+					echo round((array_sum($opgavecijfer) / count(array_filter($opgavecijfer))),1);
+					echo '</td><tr>';
+					$opgaves++;
+					$cesuurscores = array();
+					$aantalpunten = array();
+					$opgavecijfer = array();
+				}
+
+				if ($oefenopgave['bloknr'] != $blok) {
+					echo '<tr><td><i>Cijfer: '. round((array_sum($blokcijfer) / count(array_filter($blokcijfer))),1) . '</i></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td><tr>';
+					$blok++;
+					$opgaves = 1;
+					$blokcijfer = array();
+				}
+				
+
+				if (!in_array($oefenopgave['opgavenaam'], $opgavenaam)) {
+					echo '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td><td><hr></td><tr>';
+				}
+
+				echo ' <tr style="border:1px solid black;">';
+				echo '<td >'.$oefenopgave['bloknr'].'</td>';
+
+				echo '<td >'.$oefenopgave['domein'].'</td>
+
+				<td > ';
+				echo $oefenopgave['opgavenr'];
+				echo '</td>
+
+				<td>';
+				echo $oefenopgave['vraagnr_bundel'];
+				echo '</td>
+
+				<td>';
+				echo $oefenopgave['punten'];
+				echo '</td>
+
+				<td>';
+				if (isset($results[$symbol]['blok'.$blok]['oefenopgave'.$opgaves]['punten'][$oefenopgave['vraagnr_bundel']])) {
+					$behaald = $results[$symbol]['blok'.$blok]['oefenopgave'.$opgaves]['punten'][$oefenopgave['vraagnr_bundel']];
+					echo $behaald;
+				}
+				else {
+					echo '-';
+				}
+
+				echo '</td>
+
+				<td>';
+				echo $oefenopgave['cesuurscore'];
+
+				$cijfer = (1+($behaald/$oefenopgave['punten']*9));
+
+				echo '</td><td>'.$cijfer.'</td>
+			
+				<td>';
+				$opgave_naam = $oefenopgave['opgavenaam'];
+				//$opgave_nr = '-'.$oefenopgave['opgavenr_examen'];
+				echo '</td>
+				</tr>';
+				array_push($opgavenaam, $oefenopgave['opgavenaam']);
+				array_push($cesuurscores, $oefenopgave['cesuurscore']);
+				array_push($aantalpunten, $oefenopgave['punten']);
+				array_push($opgavecijfer, $cijfer);
+				array_push($blokcijfer, $cijfer);
+				//$opgave_nr = 0;
+			}
+			?>
+	</tbody>
+
+</table>
+	
+
+<style>
+	* {
+		font-family: 'Montserrat';
+		text-align:left;
+		padding-left:10px;
+	}
+	th:nth-child(1) {
+		width: 140px;
+	}
+
+	th:nth-child(2) {
+		width: 200px;
+	}
+	th:nth-child(3) {
+		width: 150px;
+	}
+	th:nth-child(4) {
+		width: 160px;
+	}
+
+	th:nth-child(5) {
+		width: 160px;
+	}
+	th:nth-child(6) {
+		width: 160px;
+	}
+	th:nth-child(7) {
+		width: 160px;
+	}
+	th:nth-child(8) {
+		width: 40px;
+	}
+	th:nth-child(9) {
+		width: 60px;
+	}
+</style>
